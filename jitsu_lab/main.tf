e68b61777db78504a686681a726e5ad3526a935f
Ivan Matveev <imatveev@delimobil.ru>
2023-08-14 17:21:09 +0300

    edit config for jitsu lab

--- jitsu_lab/main.tf
+++ jitsu_lab/main.tf
@@ -21,7 +21,7 @@ resource "yandex_vpc_subnet" "default_subnet" {
 // Create SA
 resource "yandex_iam_service_account" "sa" {
   folder_id = var.folder_id
-  name      = "airbyte-storage"
+  name      = "sql-storage"
 }
 
 // Grant permissions
@@ -37,51 +37,6 @@ resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
   description        = "static access key for object storage"
 }
 
-// Use keys to create bucket
-resource "yandex_storage_bucket" "mds_lab" {
-  access_key    = yandex_iam_service_account_static_access_key.sa-static-key.access_key
-  secret_key    = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
-  bucket_prefix = "mds-lab"
-  acl           = "public-read"
-  max_size      = 32212254720
-  force_destroy = true
-}
-
-resource "yandex_compute_instance" "airbyte" {
-  name        = "airbyte"
-  platform_id = "standard-v3"
-  zone        = yandex_vpc_subnet.default_subnet.zone
-
-  resources {
-    cores         = 4
-    memory        = 8
-    core_fraction = 100
-  }
-
-  boot_disk {
-    auto_delete = true
-    initialize_params {
-      image_id = "fd8linvus5t2ielkr8no" # with Airbyte installed
-      #   image_id = "fd80o2eikcn22b229tsa" # Container-optimized image
-      size = 30
-      type = "network-ssd"
-    }
-  }
-
-  network_interface {
-    subnet_id = yandex_vpc_subnet.default_subnet.id
-    ipv4      = true
-    nat       = true
-  }
-
-  scheduling_policy {
-    preemptible = true
-  }
-
-  metadata = {
-    user-data = "${file("cloud-init.yaml")}"
-  }
-}
 
 resource "yandex_compute_instance" "mssql" {
   name        = "mssql"
@@ -119,7 +74,7 @@ resource "yandex_compute_instance" "mssql" {
 }
 
 resource "yandex_mdb_clickhouse_cluster" "clickhouse_dwh" {
-  name                    = "clickhouse_starschema"
+  name                    = "clickhouse_dwh"
   environment             = "PRESTABLE"
   network_id              = yandex_vpc_network.default_network.id
   sql_database_management = true
