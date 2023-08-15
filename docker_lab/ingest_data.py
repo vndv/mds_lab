85b71d762267007d664c62619e03f3b4043457e4
Ivan Matveev <imatveev@delimobil.ru>
2023-08-15 15:34:59 +0300

    edit docker lab

--- docker_lab/ingest_data.py
+++ docker_lab/ingest_data.py
@@ -1,33 +1,24 @@
 import pandas as pd
+import os
 import pyarrow.parquet as pq
 from sqlalchemy import create_engine
-from tqdm import tqdm
 from time import time
 
-# df = pd.read_parquet('filename.parquet')
-# df.to_csv('filename.csv')
-
-trips = pq.read_table('yellow_tripdata_2023-05.parquet')
-trips = trips.to_pandas()
-
-# print(pd.io.sql.get_schema(trips,'yellow_taxi_data'))
-
-engine = create_engine('postgresql://imatveev:default@localhost:5432/imatveev')
-
-trips.head(n=0).to_sql(name='yellow_taxi_data',con=engine, if_exists='replace')
-
-
-# trips.to_sql(name='yellow_taxi_data',con=engine, if_exists='append')
+os.system(f"wget {url_trip}")
 
 parquet_file = pq.ParquetFile('yellow_tripdata_2023-05.parquet')
 
-for batch in tqdm(parquet_file.iter_batches(batch_size = 100000)):
+trips = parquet_file.read().to_pandas()
+
+engine = create_engine('postgresql://root:root@localhost:5433/ny_taxi')
+
+trips.head(n=0).to_sql(name='yellow_taxi_data',con=engine, if_exists='replace',index=False)
+
+for batch in parquet_file.iter_batches(batch_size = 100000):
+    t_start = time()
     batch_df = batch.to_pandas()
-    batch_df.to_sql(name='yellow_taxi_data',con=engine, if_exists='append')
-
-
-
-
-
+    batch_df.to_sql(name='yellow_taxi_data',con=engine, if_exists='append',index=False)
+    t_end = time()
+    print("inserted next chunck.. %.3f seconds" % (t_end - t_start))
 
 
