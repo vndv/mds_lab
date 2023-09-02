ff6900aca43814a7c16bf6e2302f8c6150216e15
Матвеев Иван Андреевич <iamatveev@MacBook-Air-Itsupport.local>
2023-09-02 22:03:52 +0300

    not done

--- airflow_lab/dags/ingestion_script.py
+++ airflow_lab/dags/ingestion_script.py
@@ -6,19 +6,21 @@ from time import time
 
 
 def ingest_data(table_name,pq_file):
-
-    trips_data = pq.read_table(pq_file)
-    trips = trips_data.to_pandas()
-
     engine = create_engine(f'postgresql://root:root@de_postgres:5432/ny_taxi')
-    trips = trips.rename(columns={'airport_fee': 'airport_Fee'})
-    trips.head(n=0).to_sql(name=table_name,con=engine, if_exists='replace')
     parquet_file = pq.ParquetFile(pq_file)
+
+    col_name  = 'airport_fee'
+    new_col_name = 'airport_Fee'
 
     for batch in parquet_file.iter_batches(batch_size = 100000):
         t_start = time()
+        
         batch_df = batch.to_pandas()
-        batch_df = batch_df.rename(columns={'airport_fee': 'airport_Fee'})
+
+        if col_name in batch_df.columns:
+            batch_df = batch_df.rename(columns={'airport_fee': 'airport_Fee'}, inplace=True)
+            print (f'Column {col_name} was renamed to {new_col_name}')
+        
         batch_df.to_sql(name=table_name,con=engine, if_exists='append')
         t_end = time()
         print("inserted next chunck.. %.3f seconds" % (t_end - t_start))
