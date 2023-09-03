77f675d90e8058c1d521727fa61596837e91e003
Матвеев Иван Андреевич <iamatveev@MacBook-Air-Itsupport.local>
2023-09-03 17:50:14 +0300

    format scripts

--- airflow_lab/dags/data_ingestion.py
+++ airflow_lab/dags/data_ingestion.py
@@ -11,33 +11,30 @@ AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
 
 # https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-01.parquet
 
-URL_PREFIX = 'https://d37ci6vzurychx.cloudfront.net/trip-data'
-URL_TEMPLATE = URL_PREFIX + '/yellow_tripdata_{{ execution_date.strftime(\'%Y-%m\') }}.parquet'
-OUTPUT_TEMPLATE = AIRFLOW_HOME + '/output_{{ execution_date.strftime(\'%Y-%m\') }}.parquet'
-TABLE_NAME_TEMPLATE = 'yellow_taxi_{{ execution_date.strftime(\'%Y_%m\') }}'
+URL_PREFIX = "https://d37ci6vzurychx.cloudfront.net/trip-data"
+URL_TEMPLATE = (
+    URL_PREFIX + "/yellow_tripdata_{{ execution_date.strftime('%Y-%m') }}.parquet"
+)
+OUTPUT_TEMPLATE = (
+    AIRFLOW_HOME + "/output_{{ execution_date.strftime('%Y-%m') }}.parquet"
+)
+TABLE_NAME_TEMPLATE = "yellow_taxi_{{ execution_date.strftime('%Y_%m') }}"
 
 workflow = DAG(
     "IngestionDag",
     schedule_interval="0 6 2 * *",
-    start_date=datetime(2023,1,1),
-
+    start_date=datetime(2023, 1, 1),
 )
 
 with workflow:
-
     wget_task = BashOperator(
-        task_id='wget',
-        bash_command=f'curl -sSL {URL_TEMPLATE} > {OUTPUT_TEMPLATE}'
+        task_id="wget", bash_command=f"curl -sSL {URL_TEMPLATE} > {OUTPUT_TEMPLATE}"
     )
 
     ingest_task = PythonOperator(
-        task_id='ingest',
+        task_id="ingest",
         python_callable=ingest_data,
-        op_kwargs=dict(
-            pq_file=OUTPUT_TEMPLATE,
-            table_name=TABLE_NAME_TEMPLATE
-
-        )
+        op_kwargs=dict(pq_file=OUTPUT_TEMPLATE, table_name=TABLE_NAME_TEMPLATE),
     )
 
     wget_task >> ingest_task
