7adcb1bcef63b7ecbcb464dbc57b489433f6254d
Матвеев Иван Андреевич <iamatveev@MacBook-Air-Itsupport.local>
2023-09-03 18:03:02 +0300

    formating script

--- airflow_lab/dags/ingestion_script.py
+++ airflow_lab/dags/ingestion_script.py
@@ -5,22 +5,24 @@ from sqlalchemy import create_engine
 from time import time
 
 
-def ingest_data(table_name,pq_file):
-    engine = create_engine(f'postgresql://root:root@de_postgres:5432/ny_taxi')
+def ingest_data(table_name, pq_file):
+    engine = create_engine(f"postgresql://root:root@de_postgres:5432/ny_taxi")
     parquet_file = pq.ParquetFile(pq_file)
 
-    col_name  = 'airport_fee'
-    new_col_name = 'airport_Fee'
+    col_name = "airport_fee"
+    new_col_name = "airport_Fee"
 
-    for batch in parquet_file.iter_batches(batch_size = 100000):
+    for batch in parquet_file.iter_batches(batch_size=100000):
         t_start = time()
-        
+
         batch_df = batch.to_pandas()
 
         if col_name in batch_df.columns:
-            batch_df = batch_df.rename(columns={'airport_fee': 'airport_Fee'}, inplace=True)
-            print (f'Column {col_name} was renamed to {new_col_name}')
-        
-        batch_df.to_sql(name=table_name,con=engine, if_exists='append')
+            batch_df = batch_df.rename(
+                columns={"airport_fee": "airport_Fee"}, inplace=True
+            )
+            print(f"Column {col_name} was renamed to {new_col_name}")
+
+        batch_df.to_sql(name=table_name, con=engine, if_exists="append")
         t_end = time()
         print("inserted next chunck.. %.3f seconds" % (t_end - t_start))
